d5abaee77e615b218d45b9134641791a3912f24d
Natalia Lebedeva <166998288+nativenat@users.noreply.github.com>
2024-04-27 02:40:30 +0200

    Add files via upload

---
+++ autotest.py
@@ -0,0 +1,14 @@
+# Автотест
+def test_order_creation_and_retrieval():
+    response = create_order(data.order_body)
+
+    track_number = response.json()["track"]
+    print("Заказ создан. Номер трека:", track_number)
+
+    # Получение данных заказа по треку
+    order_response = get_order(track_number)
+
+    assert order_response.status_code == 200, f"Ошибка: {order_response.status_code}"
+    order_data = order_response.json()
+    print("Данные заказа:")
+    print(order_data)
