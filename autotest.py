9d5cacef195d7af41ad870041fc225cfa0921b5a
Natalia Lebedeva <166998288+nativenat@users.noreply.github.com>
2024-04-27 12:20:26 +0200

    Update autotest.py

--- autotest.py
+++ autotest.py
@@ -9,12 +9,9 @@ def test_order_creation_and_retrieval():
     response = sendor_stand_request.create_order(data.order_body)
 
     track_number = response.json()["track"]
-    print("Заказ создан. Номер трека:", track_number)
 
     # Получение данных заказа по треку
     order_response = sendor_stand_request.get_order(track_number)
 
     assert order_response.status_code == 200, f"Ошибка: {order_response.status_code}"
     order_data = order_response.json()
-    print("Данные заказа:")
-    print(order_data)
